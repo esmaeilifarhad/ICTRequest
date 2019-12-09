5e0dccd2f6de0509974827a48f0a4a100d95db3b
Esmaeili <Esmaeili.Farhad@golrang.com>
2019-12-09 15:10:28 +0330

    980918 15:10

--- SiteAssets/JS/Equ_Task.js
+++ SiteAssets/JS/Equ_Task.js
@@ -4,6 +4,8 @@ var CurrentPID = 0;
 var CurrentName = ""
 var CurrentDep = ""
 var CurrentPLoginName = ""
+var today = "";
+var _exchangeRate = 0
 
 var _UserInGroupos = []
 var _checkedItem = []
@@ -15,6 +17,8 @@ GIG_equ_Details
 GIG_equ_Request 
 GIG_Equ_Confirm 
 GIG_Equ_Log
+GIG_equ_Policy
+Equ_GenLookUp 
 */
 $(document).ready(function () {
     //-----npm initial header Request
@@ -30,12 +34,15 @@ $(document).ready(function () {
     CurrentDep = sessionStorage.getItem("DName");
     CurrentPLoginName = sessionStorage.getItem("CurrentPLoginName");
 
-    /*
-    for (let index = 1300; index < 1370; index++) {
-        deleteRecord(index);
-       
-    }
-    */
+
+    const m = moment();
+    today = moment().format('jYYYY/jM/jD');//Today
+
+    var todayarray = today.split("/")
+    mounth = (parseInt(todayarray[1]) <= 9) ? "0" + parseInt(todayarray[1]) : parseInt(todayarray[1])
+    rooz = (parseInt(todayarray[2]) <= 9) ? "0" + parseInt(todayarray[2]) : parseInt(todayarray[2])
+    year = todayarray[0].substring(2, 4)
+    today = year + "" + mounth + "" + rooz
 
     showCartabl();
 
@@ -141,7 +148,7 @@ function Get_Details(usersInConfirm) {
         //console.log(filterStatusWF);
         $pnp.sp.web.lists.
             getByTitle("GIG_equ_Details").
-            items.select("MasterId/Id,MasterId/Title,MasterId/CID,MasterId/PersonelId,MasterId/DepName,MasterId/RequestDate,Id,Title,step,NameKalaValue,Tozihat,StatusWF,NameKala").
+            items.select("MasterId/Id,MasterId/Title,MasterId/CID,MasterId/PersonelId,MasterId/DepName,MasterId/RequestDate,Id,Title,step,NameKalaValue,Tozihat,StatusWF,NameKala,MasterId/RR_ID").
             expand("MasterId").
             // filter("(StatusWF eq 'درگردش') and (((MasterId/DepId eq 289) and (step eq 1)) or ((MasterId/DepId eq null) and (step eq 2)) or ((MasterId/DepId eq null) and (step eq 3)) or ((MasterId/DepId eq null) and (step eq 4)) or ((MasterId/DepId eq null) and (step eq 5)) or ((MasterId/DepId eq null) and (step eq 6)))")
             filter(filterStatusWF).
@@ -156,32 +163,77 @@ function Get_Details(usersInConfirm) {
             });
     });
 }
-function Create_Log(Detail,confirm) {
-    
-    debugger
-    /*
+function Create_Log(Detail, confirm, result, description) {
     return new Promise(resolve => {
         $pnp.sp.web.lists.getByTitle("GIG_Equ_Log").items.add({
-            Result:CurrentSematId,
+            Result: result,
             Title: CurrentName,
-            Dsc: CurrentPID,
-            DateConfirm:today,
-            DetailIdId: sessionStorage.getItem("UID")
+            Dsc: description,
+            DateConfirm: foramtDate(today),
+            DetailIdId: Detail.Id,
+            ConfirmIdId: confirm[0].Id
 
-            
         }).then(function (item) {
             resolve(item);
         });
-        
+
     });
-    */
+
 }
+function Get_Log(id) {
+    return new Promise(resolve => {
+        $pnp.sp.web.lists.getByTitle("GIG_Equ_Log").
+            items.
+            select("ConfirmId/Id,ConfirmId/Title,DetailId/Id,DetailId/Title,Result,Dsc,DateConfirm,Id").
+            expand("ConfirmId,DetailId").
+            filter("DetailId/Id eq " + id + "").
+            orderBy("Id", false).
+            get().
+            then(function (item) {
+                resolve(item)
+            });
+    })
+}
+function Get_Policy(Detail) {
+    return new Promise(resolve => {
+        $pnp.sp.web.lists.getByTitle("GIG_equ_Policy").
+            items.
+            select().
+            filter("(KalaValue eq " + splitString(Detail.NameKala)[0] + ") and (SemathaValue eq " + Detail.MasterId.RR_ID + ")").
+            orderBy("Id", false).
+            get().
+            then(function (item) {
+                resolve(item)
+            });
+    })
+}
+function Get_GenLookUpById(id) {
+    return new Promise(resolve => {
+        $pnp.sp.web.lists.getByTitle("Equ_GenLookUp").
+            items.
+            getById(id).get().then(function (item) {
+                resolve(item)
+            });
+    })
+
+
+    /*
+    return new Promise(resolve => {
+        $pnp.sp.web.lists.getById("4BB186CE-0D88-49DA-A252-A3295A60CFE3").items.get().
+            then(function (item) {
+                debugger
+                resolve(item)
+            });
+    })*/
+
+}
+
 function Get_DetailsById(id) {
     return new Promise(resolve => {
         $pnp.sp.web.lists.getByTitle("GIG_equ_Details").
             items.
             getById(id).
-            select("MasterId/Id,MasterId/Title,MasterId/PersonelId,MasterId/CID,MasterId/DepName,MasterId/RequestDate,Id,Title,step,NameKalaValue,Tozihat,StatusWF,NameKala").
+            select("MasterId/Id,MasterId/Title,MasterId/RR_ID,MasterId/PersonelId,MasterId/CID,MasterId/DepName,MasterId/RequestDate,Id,Title,step,NameKalaValue,Tozihat,StatusWF,NameKala,EstelamGheymat").
             expand("MasterId").
             get().
             then(function (item) {
@@ -217,45 +269,45 @@ function Get_ConfirmByStep(step, CID) {
             });
     });
 }
-function getCurrentUserinGroups() {
-    return new Promise(resolve => {
-        var endpointUrl = _spPageContextInfo.webServerRelativeUrl + '/_api/web/currentuser/?$expand=groups';
-        return $.ajax({
-            url: endpointUrl,
-            method: "GET",
-            contentType: "application/json;odata=verbose",
-            headers: { "Accept": "application/json;odata=verbose" },
-            success: function (data) {
-
-                for (let index = 0; index < data.d.Groups.results.length; index++) {
-                    _UserInGroupos.push({ ID: data.d.Groups.results[index].Id, Title: data.d.Groups.results[index].Title })
-                    // resolve(userGroups)
-                    // console.log(userGroups[index].Title+" - "+userGroups[index].Id);
-                    //  console.log("***********************");
-                }
-                resolve(_UserInGroupos)
-            },
-            error: function (data) {
-                console.log(JSON.stringify(data));
-            }
-
-        });
-    })
-}
-async function update_Details(_CurrentIdDetail, result, description, actionUser, Detail) {
+async function update_Details(_CurrentIdDetail, result, description, actionUser, Detail, Confirm, EstelamGheymat) {
     var StatusWF = Detail.StatusWF
     var varStep = Detail.step
     var DarkhastSN = Detail.DarkhastSN
 
-    var confirm = await Get_ConfirmByStep(Detail.step, Detail.MasterId.CID)
 
-    var Detail = await Create_Log(Detail,confirm)
-
+    var Policy = await Get_Policy(Detail)
+    var PriceKala = _exchangeRate * Policy[0].Price
+    debugger
     if (result == "تایید") {
-        if (confirm[0].IsFinish == true) {
+        if (Confirm[0].Role == "ICT") {
+            if (parseInt(EstelamGheymat) > PriceKala) {
+
+                var res = confirm("با توجه به اینکه قیمت کالا از محدوده درخواست کاربر بیشتر میباشد مدیر عامل باید تصمیم بگیرند")
+                if (res == true) {
+                    varStep = Detail.step + 1
+                }
+                else {
+                    $.LoadingOverlay("hide");
+                    return;
+                }
+            }
+            else {
+                /*
+                +2 جمع میشود که مرحله مدیر عامل رد شود
+                */
+                varStep = Detail.step + 2
+                DarkhastSN = await serviceICTRequestTadarokat(Detail.MasterId.RequestDate, _CurrentIdDetail);
+            }
+
+        }
+        else if (Confirm[0].Role == "AML") {
+            varStep = Detail.step + 1
+            DarkhastSN = await serviceICTRequestTadarokat(Detail.MasterId.RequestDate, _CurrentIdDetail);
+        }
+        else if (Confirm[0].IsFinish == true) {
             StatusWF = "خاتمه یافته"
             varStep = Detail.step + 1
-            var DarkhastSN = await serviceICTRequestTadarokat(Detail.MasterId.RequestDate, _CurrentIdDetail);
+            // var DarkhastSN = await serviceICTRequestTadarokat(Detail.MasterId.RequestDate, _CurrentIdDetail);
         }
         else {
             varStep = Detail.step + 1
@@ -277,12 +329,14 @@ async function update_Details(_CurrentIdDetail, result, description, actionUser,
         //  StatusWF = "درگردش";
     }
 
+    var Log = await Create_Log(Detail, Confirm, result, description)
     return new Promise(resolve => {
         var list = $pnp.sp.web.lists.getByTitle("GIG_equ_Details");
         list.items.getById(_CurrentIdDetail).update({
             step: varStep,
             StatusWF: StatusWF,
-            DarkhastSN: DarkhastSN
+            DarkhastSN: DarkhastSN,
+            EstelamGheymat: EstelamGheymat
         }).then(function (item) {
             resolve(item)
 
@@ -290,21 +344,13 @@ async function update_Details(_CurrentIdDetail, result, description, actionUser,
 
     })
 
-}
-function getGIG_MTH_DetailsById(id) {
-    return new Promise(resolve => {
-        $pnp.sp.web.lists.getByTitle("GIG_MTH_Details").items.getById(id).get().then(function (item) {
-            resolve(item);
-        });
-    });
+
 }
 //Delete
 function deleteRecord(id) {
     return new Promise(resolve => {
         var list = $pnp.sp.web.lists.getByTitle("GIG_MTH_Request");
-
         list.items.
-            // top(20).
             getById(id).
             delete().
             then(function (item) {
@@ -316,7 +362,7 @@ function deleteRecord(id) {
 }
 
 //-----------------------------
-async function confirm() {
+async function confirmForm() {
     $(this).prop('disabled', true);
     $.LoadingOverlay("show");
 
@@ -337,7 +383,7 @@ async function confirm() {
     $.LoadingOverlay("hide");
 
 }
-async function reject() {
+async function rejectForm() {
     $("#tableres2 table tr td input").each(function () {
         if ($(this).context.checked == true) {
             _checkedItem.push({ ID: $(this).attr("data_id") })
@@ -351,9 +397,15 @@ async function reject() {
     showCartabl();
 }
 async function save() {
-    var description = $("#description").val();
-    var result = $("input[name='decide']:checked").val();
+
+    var description = ""
+    description += $("#windowICT .table .description").val();
+    description += $("#window .table .description").val()
+
+    var result = $("input[name='decide']:checked").val()
     var actionUser = _spPageContextInfo.userId;
+    var EstelamGheymat = $("#EstelamGheymat").val()
+
 
     if (result == undefined) {
         alert("لطفا نتیجه را مشخص نمایید");
@@ -363,18 +415,29 @@ async function save() {
         $.LoadingOverlay("show");
 
         var Detail = await Get_DetailsById(_CurrentIdDetail)
-        var DetailRes = await update_Details(_CurrentIdDetail, result, description, actionUser, Detail)
+        if (EstelamGheymat == null || EstelamGheymat == "") {
+            EstelamGheymat = Detail.EstelamGheymat
+        }
+        var confirm = await Get_ConfirmByStep(Detail.step, Detail.MasterId.CID)
+        var DetailRes = await update_Details(_CurrentIdDetail, result, description, actionUser, Detail, confirm, EstelamGheymat)
 
         // var res=await serviceICTRequestTadarokat("980905","254");
 
-
         showCartabl();
-        $("#window").data("kendoWindow").close();
+        if (confirm[0].Role == "ICT") {
+            $("#windowICT").data("kendoWindow").close();
+        }
+        else if(confirm[0].Role == "JAM"){
+            $("#windowJAM").data("kendoWindow").close();
+        }
+        else {
+            $("#window").data("kendoWindow").close();
+        }
         $.LoadingOverlay("hide");
-
     }
 
-    //alert("Save")
+
+
 }
 async function selectAllchk(s) {
     var res = $(s)[0].checked;
@@ -392,12 +455,35 @@ async function selectAllchk(s) {
 
 }
 async function Show(id) {
-
     _CurrentIdDetail = id
     var Detail = await Get_DetailsById(id)
     var confirm = await Get_ConfirmByStep(Detail.step, Detail.MasterId.CID)
+    var Log = await Get_Log(_CurrentIdDetail)
+    var Policy = await Get_Policy(Detail)
+    var GenLookUp = await Get_GenLookUpById(1)
+    _exchangeRate = parseInt(GenLookUp.value)
+    var table = "<table class='table'>"
+    table += "<tr><th>واحد</th><th>تاریخ</th><th>نتیجه</th><th>توضیحات</th></tr>"
+    for (let index = 0; index < Log.length; index++) {
+        table += "<tr>"
+        table += "<td>" + Log[index].ConfirmId.Title + "</td>"
+        table += "<td>" + Log[index].DateConfirm + "</td>"
+        table += "<td>" + Log[index].Result + "</td>"
+        table += "<td>" + Log[index].Dsc + "</td>"
+        table += "</tr>"
+    }
+    table += "</table>"
 
+    $("#FolderLog table").remove();
+    $("#FolderLogICT table").remove();
+    $("#FolderLogJAM table").remove();
 
+    $("#FolderLog").append(table);
+    $("#FolderLogICT").append(table);
+    $("#FolderLogJAM").append(table);
+
+    $(".Price span").remove();
+    $(".Confirm span").remove();
     $(".PersonelId span").remove();
     $(".NamePersonel span").remove();
     $(".DepName span").remove();
@@ -406,6 +492,9 @@ async function Show(id) {
     $(".NameKala span").remove();
 
 
+    $("#EstelamGheymat").val(Detail.EstelamGheymat);
+    $(".Price").append("<span>" + SeparateThreeDigits(parseInt(GenLookUp.value) * parseInt(Policy[0].Price)) + "</span>");
+    $(".Confirm").append("<span>" + confirm[0].Title + "</span>");
     $(".PersonelId").append("<span>" + Detail.MasterId.PersonelId + "</span>");
     $(".NamePersonel").append("<span>" + Detail.MasterId.Title + "</span>");
     $(".DepName").append("<span>" + Detail.MasterId.DepName + "</span>");
@@ -416,6 +505,9 @@ async function Show(id) {
 
     if (confirm[0].Role == "ICT") {
         showWindowsICT()
+    }
+    if (confirm[0].Role == "JAM") {
+        showWindowsJAM()
     }
     else {
         showWindows();
@@ -459,10 +551,27 @@ function showWindowsICT() {
         }
     }).data("kendoWindow").center().open();
 }
+function showWindowsJAM() {
+    var myWindow = $("#windowJAM"),
+        undo = $("#newRecord");
+    myWindow.kendoWindow({
+        width: "1200px",
+        title: "فرم تایید کالا",
+        visible: false,
+        actions: [
+            // "Pin",
+            // "Minimize",
+            //"Maximize",
+            "Close"
+        ],
+        close: function () {
+            undo.fadeIn();
+        }
+    }).data("kendoWindow").center().open();
+}
 //-------------------------------------------web services
 //header master
 function serviceICTRequestTadarokat(myDate, PortalReqHeaderID) {
-    debugger
     return new Promise(resolve => {
         var serviceURL = "https://portal.golrang.com/_vti_bin/SPService.svc/ICTRequestTadarokat"
         var request = { CID: CurrentCID, Date: myDate, PortalReqHeaderID: PortalReqHeaderID }
@@ -483,6 +592,8 @@ function serviceICTRequestTadarokat(myDate, PortalReqHeaderID) {
                 }
                 else {
                     alert(data)
+                    alert("لطفا با واحد پرتال تماس بگیرید" + "\n" + "کد مقابل را جهت پیگیری ارائه دهید" + _CurrentIdDetail)
+                    $.LoadingOverlay("hide");
                 }
             },
             error: function (a) {
@@ -531,10 +642,23 @@ function calDayOfWeek(date) {
     }
     return day;
 }
+//980809  input parameter
 function foramtDate(str) {
-    return str.slice(0, 2) + "/" + str.slice(2, 4) + "/" + str.slice(4, 6)
+    if (str.length == 6) {
+        return "13" + str.slice(0, 2) + "/" + str.slice(2, 4) + "/" + str.slice(4, 6)
+    }
+
 }
 function splitString(str) {
     return str.split(";#")
 }
+//سه رقم سه رقم جدا کنه برای پول   SeparateThreeDigits
+function SeparateThreeDigits(str) {
+    var x = parseInt(str);
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    // return parseInt(str);
+
+}
+
 //-----------------------
